5f9965023330c953f0dd9b935fec99b4ee997a74
joseluis.geronimo@us.crawco.com <joseluis.geronimo@us.crawco.com>
2023-08-02 19:42:49 +0000

    5

--- models/as_of_date.sql
+++ models/as_of_date.sql
@@ -1,5 +1,13 @@
+{{ config(materialized='table') }}
+
+{%- set datepart = "day" -%}
+{%- set start_date = "TO_DATE('2021/01/01', 'yyyy/mm/dd')" -%}
+{%- set end_date = "TO_DATE('2021/04/01', 'yyyy/mm/dd')" -%}
+
 WITH as_of_date AS (
-    {{ dbt_utils.date_spine(datepart, start_date, end_date) }}
+    {{ dbt_utils.date_spine(datepart=datepart, 
+                            start_date=start_date,
+                            end_date=end_date) }}
 )
 
 SELECT DATE_{{datepart}} as AS_OF_DATE FROM as_of_date
