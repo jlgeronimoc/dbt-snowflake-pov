8569996594cb8042e35b7543479e74b4268d7bf1
joseluis.geronimo@us.crawco.com <joseluis.geronimo@us.crawco.com>
2023-08-03 15:49:59 +0000

    move examples from RiskTech POV

--- models/d__business_vault/RiskTech/PIT/as_of_date.sql
+++ models/d__business_vault/RiskTech/PIT/as_of_date.sql
@@ -4,10 +4,10 @@
 {%- set start_date = "TO_DATE('2021/01/01', 'yyyy/mm/dd')" -%}
 {%- set end_date = "TO_DATE('2021/04/01', 'yyyy/mm/dd')" -%}
 
-WITH AS_OF_DATE AS (
+WITH as_of_date AS (
     {{ dbt_utils.date_spine(datepart=datepart, 
                             start_date=start_date,
                             end_date=end_date) }}
 )
 
-SELECT DATE_{{datepart}} as AS_OF_DATE FROM AS_OF_DATE
+SELECT DATE_{{datepart}} as AS_OF_DATE FROM as_of_date
